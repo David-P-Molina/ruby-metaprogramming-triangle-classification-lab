66333b42888a2a64a20aa1d47d8f4179f2925666
David Moina <davemolina777@gmail.com>
2020-11-20 10:47:04 -0600

    commit tree 98b4740aa4eb2e0db68406d057b664c32355a64c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require "pry"
 class Triangle
   # write code here
+  attr_accessor :s1, :s2, :s3, :equilateral, :isosceles, :scalene
+  def initialize(s1,s2,s3)
+    @s1= s1
+    @s2 = s2 
+    @s3 = s3
+  end
+  
+  def kind
+          if (s1 <= 0) || (s2 <= 0) || (s3 <= 0)
+            raise TriangleError
+          elsif (s1 + s2 <= s3) || (s2 + s3 <= s1)  || (s1 + s3 <= s2)
+            raise TriangleError
+          elsif s1 == s2 && s2 == s3 && s1 > 0
+           :equilateral
+         elsif (s2 == s3 && s1 > 0 && s3 > 0) || (s1 == s2 && s1 > 0 && s3 > 0 ) || (s3 == s1 && s1 > 0 && s3 > 0 )
+           :isosceles
+         elsif (s1 != s2 && s2 != s3 && s1 != s3)
+           :scalene
+          # binding.pry
+         end
+  end
+  class TriangleError < StandardError
+    def message
+    end
+  end
+
 end
+
